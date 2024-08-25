6276c66e6019dfabbc550b8b0d39d3a7693e7e76
Matthew Chak <physboom@Matthews-MBP.lan>
2024-08-25 11:49:43 -0700

    fix: changed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mmdcritic',
+    name='mmd_critic',
     version='0.1.0',
     description='Python package for the MMD Critic method',
     long_description=open('README.md').read(),

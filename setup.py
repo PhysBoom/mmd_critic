15a9e98f4ff3b5f9b0a5538f25900c08e839e09f
PhysBoom <matthew@teleparty.com>
2025-01-01 17:46:47 -0800

    chore: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mmd-critic',
-    version='0.1.1',
+    version='0.1.2',
     description='Python package for the MMD Critic method',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
